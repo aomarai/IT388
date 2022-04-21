6c8318bfd49dfce3aa639aa48a4d2df669a45525
Ash Omaraie <aomarai@ilstu.edu>
2022-04-21 15:31:29 -0500

    Trying to get program to not seg fault on Expanse

--- Homework 5/cuda_matrixAdd.cu
+++ Homework 5/cuda_matrixAdd.cu
@@ -9,6 +9,15 @@ __global__ void addMatrix(int A[], int B[], int C[], int m, int n)
 {
    int i= blockDim.x * blockIdx.x + threadIdx.x;
 
+   if(i < m)
+   {
+      int j;
+      for(j=0; j<n; j++)
+      {
+         C[i*n+j] = A[i*n+j] + B[i*n+j];
+      }
+   }
+
    if (blockIdx.x < m && threadIdx.x < n) 
       C[i] = A[i] + B[i];
 }
@@ -17,14 +26,12 @@ __global__ void addMatrix(int A[], int B[], int C[], int m, int n)
 void generateMatrices(int A[], int B[], int m, int n)
 {
    int i, j;
-   for (i = 0; i < m; i++)
-   {
-      for (j = 0; j < n; j++)
+   for (i=0; i<m; i++)
+      for (j=0; j<n; j++)
       {
-         A[i * n + j] = i * j;
-         B[i * n + j] = i * j;
+         A[i*n+j] = i+j;
+         B[i*n+j] = i+j;
       }
-   }
 }
 
 void printMatrix(int A[], int m, int n)
@@ -45,7 +52,6 @@ void printMatrix(int A[], int m, int n)
 //     return t.tv_sec + t.tv_usec / 1000000.0;
 // }
 
-/* Host code */
 int main(int argc, char *argv[])
 {
    int m, n;
@@ -55,20 +61,20 @@ int main(int argc, char *argv[])
    int blockSize, numBlocks;
 
    //Get size of matrices
-   if (argc != 3)
+   if (argc != 4)
    {
-      fprintf(stderr, "usage: %s <matrix size> <threads per block>\n", argv[0]);
+      fprintf(stderr, "usage: %s <matrix height> <matrix width> <threads per block>\n", argv[0]);
       exit(0);
    }
-   m = n = atoi(argv[1]);
-   blockSize = atoi(argv[2]);
+   m = atoi(argv[1]);
+   n = atoi(argv[2]);
+   blockSize = atoi(argv[3]);
    numBlocks = ceil(double(n) / blockSize);
-   printf("Matrix Sizes: %d Block Size: %d\n", m, blockSize);
    size = m * n * sizeof(int);
 
-   h_A = (int *)malloc(size);
-   h_B = (int *)malloc(size);
-   h_C = (int *)malloc(size);
+   cudaMallocManaged(&h_A, size);
+   cudaMallocManaged(&h_B, size);
+   cudaMallocManaged(&h_C, size);
 
    generateMatrices(h_A, h_B, m, n);
 
@@ -81,16 +87,34 @@ int main(int argc, char *argv[])
    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 
+   //Grab starting time
+   // double start, elapsed;
+   // start = getTime();
+
    addMatrix<<<numBlocks, blockSize>>>(d_A, d_B, d_C, m, n);
 
    //Wait for the kernel to complete
    cudaDeviceSynchronize();
 
+   //Get elapsed time
+   //elapsed = getTime() - start;
+   
    //Copy result from device memory to host memory
    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
-   printf("Sum:\n");
-   printMatrix(h_C, m, n);
+   if (m < 20 && n < 20)
+   {
+      printf("Matrix A:\n");
+      printMatrix(h_A, m, n);
+      printf("Matrix B:\n");
+      printMatrix(h_B, m, n);
+      printf("Matrix C:\n");
+      printMatrix(h_C, m, n);
+   }
+
+   //Print elapsed time
+   //printf("Elapsed time: %f\n", elapsed);
+   printf("Matrix Size: %d by %d Block Size: %d Number of Blocks: %d\n", m, n, blockSize, numBlocks);
 
    //Free device memory
    cudaFree(d_A);
